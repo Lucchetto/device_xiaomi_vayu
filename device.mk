2d97b25c602ca00783513c295b8a720410c051dd
TheHitMan7 <krtik.vrma@gmail.com>
2021-04-02 23:47:16 +0530

    vayu: Import Pixel PowerStats Util
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>
    Signed-off-by: alanndz <alanndz@qmail.id>

--- device.mk
+++ device.mk
@@ -350,6 +350,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_PACKAGES_DEBUG += \
+    pwrstats_util
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
