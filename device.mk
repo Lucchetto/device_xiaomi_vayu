49b5bc89e1a169f8ec4c2dfd269d5acfcc55b2e3
Rafiester <rafi.haqqi@gmail.com>
2021-08-04 18:54:57 +0700

    vayu: Don't build lineage specific HIDL

--- device.mk
+++ device.mk
@@ -268,10 +268,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_msmnile
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
@@ -413,10 +409,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
-
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
